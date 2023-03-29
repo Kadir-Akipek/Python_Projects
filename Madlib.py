280066528b5c660b4e5957faf68b0e85baa34e17
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-03-29 11:22:32 +0300

    Kelime oyunu

---
+++ Madlib.py
@@ -0,0 +1,15 @@
+#youtuber = "mustafa akipek"
+
+#print("abone ol" + youtuber)
+#print("abone ol {}".format(youtuber))
+#print(f"abone ol {youtuber}")
+
+sft = input("Sıfat : ")
+fiil1 = input("Fiil: ")
+fiil2 = input("Fiil: ")
+unlu_kisi = input("Ünlü kişi: ")
+
+madlib = f"Bilgisayar programlama çok {sft}! Beni her zaman çok heyecanlandırdı çünkü \
+{fiil1} seviyorum. Sulu kal ve {fiil2} {unlu_kisi} gibisin"
+
+print(madlib)
